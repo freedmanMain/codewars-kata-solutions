bfc795120751331c6a84f2a9e5a302c6ce935b72
Maksim <max.freedman.main@gmail.com>
2021-04-03 21:13:20 +0300

    solved Small Enough

--- src/main/java/solutions/kuy7/MaxProduct.java
+++ src/main/java/solutions/kuy7/MaxProduct.java
@@ -2,11 +2,11 @@ package solutions.kuy7;
 
 public class MaxProduct {
     public int adjacentElementsProduct(int[] array) {
-        int _globalMax = Integer.MIN_VALUE;
+        int globalMax = Integer.MIN_VALUE;
         for (int i = 0; i < array.length - 1; i++) {
-            int _localMax = array[i] * array[i + 1];
-            _globalMax = Math.max(_globalMax, _localMax);
+            int localMax = array[i] * array[i + 1];
+            globalMax = Math.max(globalMax, localMax);
         }
-        return _globalMax;
+        return globalMax;
     }
 }
