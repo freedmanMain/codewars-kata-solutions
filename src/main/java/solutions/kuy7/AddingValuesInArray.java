c2ff7146c7ecff995b497cc1efbaa3d5080e054b
Maksim <max.freedman.main@gmail.com>
2021-04-03 12:57:17 +0300

    solved Adding values of arrays in a shifted way

---
+++ src/main/java/solutions/kuy7/AddingValuesInArray.java
@@ -0,0 +1,15 @@
+package solutions.kuy7;
+
+public class AddingValuesInArray {
+    public static int[] addingShifted(int[][] arrayOfArrays, int shift) {
+        int[] arrayOfSums = new int[arrayOfArrays[0].length + shift * (arrayOfArrays.length - 1)];
+        int start = 0;
+        for (int i = 0; i < arrayOfArrays.length; i++) {
+            for (int j = 0; j < arrayOfArrays[i].length; j++) {
+                arrayOfSums[j + start] += arrayOfArrays[i][j];
+            }
+            start += shift;
+        }
+        return arrayOfSums;
+    }
+}
